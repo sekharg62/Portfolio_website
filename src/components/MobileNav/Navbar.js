dccd8e03082d3e266d0b628f835cb05ebaa85d8f
sekharg62 <ghoshsekhar6296@gmail.com>
2024-08-03 19:54:37 +0530

    g

--- src/components/MobileNav/Navbar.js
+++ src/components/MobileNav/Navbar.js
@@ -16,10 +16,10 @@ export default function Navbar() {
         <div className="nav-content">
             <img src={logo} alt="" className="logo" />
             <ul>
-                <li><a href="/"  className="menu-item">Home</a></li>
-                <li><a href="/" className="menu-item">Skills</a></li>
-                <li><a href="/" className="menu-item">Work Experience</a></li>
-                <li><a href="/" className="menu-item">Connnects</a></li>
+                <li><a className="menu-item">Home</a></li>
+                <li><a className="menu-item">Skills</a></li>
+                <li><a className="menu-item">Work Experience</a></li>
+                <li><a className="menu-item">Connnects</a></li>
                 <button className="contact-btn" onClick={()=>{}}>Hire Me</button>
             </ul>
             <button className="menu-btn" onClick={toggelMenu}>
