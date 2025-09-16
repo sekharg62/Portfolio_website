eb084018d7117ddd23a6a264c661fc580dec9d12
sekharg62 <ghoshsekhar6296@gmail.com>
2025-09-16 18:13:28 +0530

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,6 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className="footer">
-       @2024 Sekhar | Coyright Reserved</div>
+       @2025 Sekhar | Coyright Reserved</div>
   )
 }
